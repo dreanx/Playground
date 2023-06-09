491db02995d3bdd1616c83a47522481f41e0ccfb
dreanx <pierre.basck@gmail.com>
2023-06-09 09:17:11 +0200

    added Comments to FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offsetCamera = new Vector3(0,2,-5);
+    public Transform player; // Reference to the player object's transform
+    public Vector3 offsetCamera = new Vector3(0, 2, -5); // Offset to adjust the camera position relative to the player
 
-
-        private void Update()
+    private void Update()
     {
+        // Set the position of the camera to the player's position plus the offset
         transform.position = player.transform.position + offsetCamera;
     }
 }
